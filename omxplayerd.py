aef6ea86675778bc4668552656948bac3d9b568c
testa <testa.a.tapos@gmail.com>
2013-06-24 04:41:05 +0100

    Add new ui desing, Remove jquery, Interactive mode preparation

--- omxplayerd.py
+++ omxplayerd.py
@@ -3,6 +3,7 @@ import subprocess
 import time
 import re
 import web
+from web import form
 import os
 import pipes
 import string
@@ -13,15 +14,21 @@ urls = (
 '^/play/(.*)$','Play',
 '^/path/?(.*)$','Path',
 '^/playlist/?(.*)$','Playlist',
+'^/stop/','Stop',
+'^/pause/','Pause',
 '^/([^/]*)$','Other'
 )
 
 PLAYABLE_TYPES = ['.264','.avi','.bin','.divx','.f4v','.h264','.m4e','.m4v','.m4a','.mkv','.mov','.mp4','.mp4v','.mpe','.mpeg','.mpeg4','.mpg','.mpg2','.mpv','.mpv2','.mqv','.mvp','.ogm','.ogv','.qt','.qtm','.rm','.rts','.scm','.scn','.smk','.swf','.vob','.wmv','.xvid','.x264','.mp3','.flac','.ogg','.wav', '.flv', '.mkv']
 MEDIA_RDIR = 'media/'
 PAGE_FOLDER = 'omxfront/'
-PAGE_NAME = 'interface.htm'
+PAGE_NAME = 't.html'
 OMXIN_FILE='omxin'
 
+playerstat = {}
+
+playerstat['playing'] = 0
+playerstat['pause'] = 0
 play_list = []
 
 command_send={
@@ -35,7 +42,6 @@ command_send={
 'prevsubs':'n',
 'togglesubs':'s',
 'stop':'q',
-'pause':'p',
 'volumedown':'-',
 'volumeup':'+',
 'languagedown':'j',
@@ -46,7 +52,9 @@ command_send={
 'seek+600':'\x1b\x5b\x41'}
 
 class Other:
-    def GET(self,name):
+    def POST(self,name):
+        inpury = web.input(kola = 'web')
+        print(inpury.kola)
         if not name == '':
             if name in command_send:
                 omx_send(command_send[name])
@@ -58,13 +66,54 @@ class Other:
                     page_file.close()
                     return page
                 return '[{\"message\":\"FAIL\"}]'
+        inpury = web.input(kola = 'web')
+        print(inpury.kola)
         print('Incorrect capture!')
         return '[{\"message\":\"ERROR!!!\"}]'
 
+class Pause:
+    def POST(self):
+        global playerstat
+        if playerstat['pause'] == 1:
+            omx_send("p")           
+            web.header('Content-Type', 'text/json')
+            playerstat['pause'] = 0
+            return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"playing\"}]'
+        else:
+            omx_send("p")  
+            web.header('Content-Type', 'text/json')
+            playerstat['pause'] = 1 
+            return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"pause\"}]'
+
+
 class Play:
-    def GET(self,file):
-        omx_play(file)
-        return '[{\"message\":\"OK\"}]'
+    def POST(self,file):
+        global playerstat
+        if playerstat['playing'] == 1:
+            if playerstat['pause'] == 1:
+                omx_send("p")           
+                web.header('Content-Type', 'text/json')
+                playerstat['pause'] = 0
+                return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"playing\"}]'
+            else:
+                omx_send("p")  
+                web.header('Content-Type', 'text/json')
+                playerstat['pause'] = 1 
+                return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"pause\"}]'
+        else:
+            omx_play(file)
+            playerstat['playing'] = 1
+            web.header('Content-Type', 'text/json')
+            return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"playing\"}]'
+
+class Stop:
+    global playerstat
+    def POST(self):
+        global playerstat
+        if playerstat['playing'] == 1:
+           omx_stop()
+           playerstat['playing'] = 0
+        return '[{\"message\":\"OK\"},{\"modul\":\"interface\",\"interfac\":\"stop\"}]'
 
 class Shutdown:
     def GET(self):
@@ -73,11 +122,41 @@ class Shutdown:
 
 class Interface:
     def GET(self):
-        page_file = open(os.path.join(PAGE_FOLDER,PAGE_NAME),'r')
-        page = page_file.read()
+        page_file = open(os.path.join(PAGE_FOLDER,"ta.html"),'r')
+        pagea = page_file.read()
+        page_file.close()
+        itemlist = []
+        path=''
+        if path.startswith('..'):
+            path = ''
+        for item in os.listdir(os.path.join(MEDIA_RDIR,path)):
+            if os.path.isfile(os.path.join(MEDIA_RDIR,path,item)):
+                fname = os.path.splitext(item)[0]
+                fname = re.sub('[^a-zA-Z0-9\[\]\(\)\{\}]+',' ',fname)
+                fname = re.sub('\s+',' ',fname)
+                fname = string.capwords(fname.strip())
+                singletuple = (os.path.join(path,item),fname,'file')
+            else:
+                fname = re.sub('[^a-zA-Z0-9\']+',' ',item)
+                fname = re.sub('\s+',' ',fname)
+                fname = string.capwords(fname.strip())
+                singletuple = (os.path.join(path,item),fname,'dir')
+            itemlist.append(singletuple)
+        itemlist = [f for f in itemlist if not os.path.split(f[0])[1].startswith('.')]
+        itemlist = [f for f in itemlist if os.path.splitext(f[0])[1].lower() in PLAYABLE_TYPES or f[2]=='dir']
+        list.sort(itemlist, key=lambda alpha: alpha[1])
+        list.sort(itemlist, key=lambda dirs: dirs[2])
+        outputlist=[]
+        kar = 0
+        for line in itemlist:
+            kar+=1
+            outputlist.append('singer.playlist['+str(kar)+'] = {\"path\":\"'+line[0]+'\", \"ame\":\"'+line[1]+'\", \"type\":\"'+line[2]+'\"};')
+        page_file = open(os.path.join(PAGE_FOLDER,"tb.html"),'r')
+        pageb = page_file.read()
         page_file.close()
         web.header('Content-Type', 'text/html')
-        return page
+        return pagea+'\n'.join(outputlist)+pageb
+        
 
 
 class Path:
@@ -130,10 +209,13 @@ def omx_send(data):
 def omx_play(file):
     #omx_send('q')
     #time.sleep(0.5) #Possibly unneeded - crashing fixed by other means.
-    subprocess.Popen('killall omxplayer.bin',stdout=subprocess.PIPE,shell=True)
+    subprocess.Popen('killall -9  omxplayer.bin',stdout=subprocess.PIPE,shell=True)
     subprocess.Popen('clear',stdout=subprocess.PIPE,shell=True)
-    subprocess.Popen('omxplayer -o hdmi '+os.path.join(MEDIA_RDIR,re.escape(file))+' <'+re.escape(OMXIN_FILE),shell=True)
+    subprocess.Popen('omxplayer -p -o hdmi '+os.path.join(MEDIA_RDIR,re.escape(file))+' <'+re.escape(OMXIN_FILE),shell=True)
     omx_send('.')
     return 1
 
 
+def omx_stop():
+    subprocess.Popen('killall -9  omxplayer.bin',stdout=subprocess.PIPE,shell=True)
+    subprocess.Popen('clear',stdout=subprocess.PIPE,shell=True)
